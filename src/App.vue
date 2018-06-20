d516a4d8f25a7f284c66824e2e0ccf9b29ef67e5
Leah Wamuyu <leah.wamuyu@yahoo.com>
2018-06-20 18:05:10 +0300

    modified webpack

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="container">
-    <h3>{{ title }}</h3>
+    <h3>{{ title }} </h3>
+    <form>
+    <textarea placeholder="Add new task" v-model="newTask"></textarea>
+    <p>{{newTask}}</p>
+    <button type="button" v-on:click="addTask">Add Task {{tasks.length}} </button>
+    </form>
+    <ul>
+    <li v-for="task in tasks">
+    {{task}}
+    </li>
+    </ul>
   </div>
 </template>
 
@@ -8,8 +18,23 @@
 export default {
   data: function() {
     return {
-      title: 'My App'
+      title: 'My Task Manager',
+      newTask:"This is a SAMPLE",
+      tasks:[
+      
+            ]
     }
+  },
+  methods: {
+  	addTask() {
+  	
+  		this.tasks.unshift(this.newTask);
+  		this.newTask = '';
+
+
+
+
+  	}
   }
 }
 </script>
